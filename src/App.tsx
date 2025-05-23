95f7070daaea5e3447a32d0db2367ff80ee57b79
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2025-05-23 21:29:38 +0900

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 export default function App() {
   return (
-    <h1 className="text-2xl font-bold text-blue-600">Hello Vite + React!</h1>
+    <>
+      <h1 className="text-2xl font-bold text-primary-500">
+        Hello Vite + React!
+      </h1>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button>Click me</Button>
+      </div>
+    </>
   );
 }
